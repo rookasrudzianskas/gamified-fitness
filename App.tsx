8ed2742ad2394142585242e6284947d702564e05
Rokas <rookas.rudzenskas@gmail.com>
2023-09-14 13:37:55 +0300

    FIX

--- App.tsx
+++ App.tsx
@@ -1,11 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+import { Camera } from 'expo-camera';
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.front);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <Camera style={{ flex: 1 }} type={type}>
+        <View style={styles.container}>
+          {/* Display your game elements here */}
+          {Array(3).fill(0).map((_, i) => (
+            <View style={styles.cameraSection} key={i}>
+              {Array(3).fill(0).map((_, j) => (
+                <View style={styles.column} key={j}></View>
+              ))}
+            </View>
+          ))}
+        </View>
+      </Camera>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            setType(
+              type === Camera.Constants.Type.back
+                ? Camera.Constants.Type.front
+                : Camera.Constants.Type.back
+            );
+          }}
+        >
+          <Text style={styles.text}>Flip</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +56,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    flexDirection: 'column', // To create columns
+    backgroundColor: 'transparent',
+  },
+  cameraSection: {
+    flex: 1,
+    flexDirection: 'row', // To create lines
+  },
+  column: {
+    flex: 1,
+    borderWidth: 2,
+    borderColor: 'red',
+  },
+  buttonContainer: {
+    flex: 0.1,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
     justifyContent: 'center',
+  },
+  button: {
+    flex: 0.1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    color: 'white',
   },
 });
